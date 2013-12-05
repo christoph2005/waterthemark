c1760ee45080ea42c5b81090df889b1df69d5214
Chris Murphy <christoph2005@yahoo.com>
2013-12-05 03:04:44 -0800

    Lets hope this works

--- index.php
+++ index.php
@@ -58,7 +58,7 @@
 				echo "Stored in: " . $_FILES["source"]["tmp_name"] . "<br>";
 				
 		 		$target_path = "./uploads/";
-				$target_path = $target_path . urlencode(basename($_FILES['source']['name']));
+				$target_path = $target_path . strtolower(urlencode(basename($_FILES['source']['name'])));
 				move_uploaded_file($_FILES['source']['tmp_name'], $target_path);
 			    echo "Stored in: " .$target_path.'<br>';
 	  
