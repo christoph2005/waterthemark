3157ef772ba25215d64cf6fe4e58a04e70bdd82c
Chris Murphy <christoph2005@yahoo.com>
2013-12-05 04:03:38 -0800

    Thank GOD\!

--- index.php
+++ index.php
@@ -68,7 +68,7 @@
 				{
 					$output;
 					exec('java -version',$output);
-					echo 'alert('.printArray($output).')<br>';
+					echo 'alert('.printArray($output).');<br>';
 				    echo "Stored in: " .$dest_path1.'<br>';	  
 					echo '<image src="'.$dest_path1. '" width = "400"></image>';
 					exec('java -cp uploads/ -jar uploads/dct-watermark-rev24.jar e -d '.$dest_path1.' '.$dest_path2.' "Hello World"');
