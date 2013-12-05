64824f280dc528658a416b82b3a59ea556a36437
Chris Murphy <christoph2005@yahoo.com>
2013-12-05 03:33:43 -0800

    Thank GOD\!

--- index.php
+++ index.php
@@ -57,14 +57,19 @@
 				echo "Size: " . ($_FILES["source"]["size"] / 1024) . " kB<br>";
 				echo "Stored in: " . $_FILES["source"]["tmp_name"] . "<br>";
 				
+				// Note to self: $destpath junk can probably be tidied up a bit
 		 		$target_path = "uploads/";
-				$target_path = $target_path . strtolower(urlencode(basename($_FILES['source']['name'])));
-				if (!move_uploaded_file($_FILES['source']['tmp_name'], $target_path))
+				$filename1 = strtolower(urlencode(basename($_FILES['source']['name'])));
+				$dest_path1 = $target_path . $filename1;
+				$dest_path2 = $target_path . 'marked.png';
+				if (!move_uploaded_file($_FILES['source']['tmp_name'], $dest_path1))
 					echo '<h1>'.$_FILES["source"]["name"].' failed to load!</h1>';
 				else
 				{
-				    echo "Stored in: " .$target_path.'<br>';	  
-					echo '<image src="'.$target_path. '" width = "400"></image><br>';
+				    echo "Stored in: " .$dest_path1.'<br>';	  
+					echo '<image src="'.$dest_path1. '" width = "400"></image><br>';
+					exec('java -jar uploads/dct-watermark-rev24.jar e -d '.$dest_path1.' '.$dest_path2.'"Hello World"');
+					echo '<image src="'.$dest_path2. '" width = "400"></image><br>';
 				}
 			}
 		 
