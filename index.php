ee616c916659b0a7a6b1543520cf24775dbd9e9a
Chris Murphy <christoph2005@yahoo.com>
2013-12-04 21:29:48 -0800

    Test

--- index.php
+++ index.php
@@ -43,7 +43,8 @@
 		         echo '</form>';
 			}
 			else {
-				echo "alert($_Files)";
+				echo "alert($_Files);";
+				echo "alert(\"Alert works\");";
 			}
          echo '</body></html>';
       }
