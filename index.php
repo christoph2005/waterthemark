8ea3004d25770e982e78cd1577f055c361f81314
Chris Murphy <christoph2005@yahoo.com>
2013-12-04 22:26:19 -0800

    Test

--- index.php
+++ index.php
@@ -33,16 +33,22 @@
 			if(empty($_POST) || empty($_FILES['source']))
 			{
 		         echo '<form id="UploadForm" enctype="multipart/form-data" action="./index.php" method="POST">';
+				 
 				 echo 'POST: '; echo empty($_POST);
 				 echo '<br>';
 				 print_r($_POST);
 				 echo '<br>';
 				 echo '<br>';
+				 echo $_POST['message'];
+				 echo '<br>';
+				 echo '<br>';
+				 
 				 echo 'FILES: '; echo empty($_FILES);
 				 echo '<br>';
 				 print_r($_FILES);
 				 echo '<br>';
 				 echo '<br>';
+				 
 			         echo 'Please choose a photo: ';
 			         echo '<input name="source" type="file"><br/><br/>';
 			         echo 'Say something about this photo: ';
