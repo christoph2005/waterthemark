91b661060ef2b5a967d89c3c2a6150837b93a3dd
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 15:19:46 -0800

    FROM SCRATCH

--- index.php
+++ index.php
@@ -3,11 +3,7 @@
 <header><div id="fb-root"></div></header>
 <body>
 <!--Your Impelementation goes below here-->		
-	<script>
-	FB.api("/me",
-		function (response){
-		alert(response.name);
-	});</script>
+
 </body>
 <footer>
 <?php echo(file_get_contents('./javascript/FBLogin.js'));	?>
