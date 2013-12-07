7342547e3b5daf7948f533493946cb8cdd1ed417
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 14:14:42 -0800

    FROM SCRATCH

--- index.php
+++ index.php
@@ -6,6 +6,6 @@
 	<script> if(FB) alert("FB object exists!");</script>
 </body>
 <footer>
-//<?php echo(file_get_contents('./javascript/FBLogin.js'));	?>
+<?php echo(file_get_contents('./javascript/FBLogin.js'));	?>
 </footer>
 </html>
