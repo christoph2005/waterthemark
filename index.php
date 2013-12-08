865590e0aec263742335ad734df983a4f4dd907c
Chris Murphy <christoph2005@yahoo.com>
2013-12-08 01:02:18 -0800

    Making the layout make more sense

--- index.php
+++ index.php
@@ -17,7 +17,7 @@
 		<input type="submit" value="Watermark this image!"/><br>
 	</form>
 <!-- Show a Post To Facebook button if you find an uploaded filename -->
-		<?php if($_FILES["FU"]["name"]): ?>
+		<?php if($dest_path2 && $_FILES["FU"]["name"]): ?>
 			<script>
 			function postToFacebook()
 			{
