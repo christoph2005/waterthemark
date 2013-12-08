75314b8f149cb271f3f77b0ccc55226e496ee94d
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 16:42:24 -0800

    Alert added to show

--- index.php
+++ index.php
@@ -4,7 +4,22 @@
 <?php echo(file_get_contents('./javascript/FBLogin.js')); ?>
 <body>
 <!--Your Impelementation goes below here-->		
-<button onclick='f()'>PUSH THIS BUTTON</button>
+<br></p><button onclick='f()'>PUSH THIS BUTTON</button>
+
+<!-- A form to upload a picture -->		 
+	<form enctype="multipart/form-data" action="./index.php" method="POST">
+		Please choose a photo: <input name="source" type="file"><br/><br/>
+		<input type="submit" value="Upload"/><br/>
+	</form>
+	
+<!-- A form to generate an HTTP Post -->
+	<form enctype="multipart/form-data" action="'.$graph_url.'" method="POST">
+		<datalist>
+		<option name="source" type="file" value="'.$_POST["source"].'"></option>
+		</datalist>
+		Say something about this photo: <input name="message" type="text" value=""><br/><br/>
+		<input type="submit" value="Post to Facebook!"/><br/>
+	</form>
 <script>
 function f(){
    FB.api('/me', function(response) {
