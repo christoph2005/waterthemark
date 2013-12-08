1e65f29ccb5a3039edd822fd0069d6f4adae16ab
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 21:48:25 -0800

    Trying to get the file to upload

--- index.php
+++ index.php
@@ -19,9 +19,36 @@
 			<br><input name='fileUpload' type="file"><br><br>
 		<input type="submit" value="Upload Image For Watermarking"/><br>
 	</form>
-	
+<?php if ($_FILES["fileUpload"]["error"] > 0)
+			{
+				echo "Error: " . $_FILES["fileUpload"]["error"] . "<br>";
+			}
+			else
+			{
+				echo "Upload: " . $_FILES["fileUpload"]["name"] . "<br>";
+				echo "Type: " . $_FILES["fileUpload"]["type"] . "<br>";
+				echo "Size: " . ($_FILES["fileUpload"]["size"] / 1024) . " kB<br>";
+				echo "Stored in: " . $_FILES["fileUpload"]["tmp_name"] . "<br>";
+				
+				// Note to self: $destpath junk can probably be tidied up a bit
+		 		$target_path = "uploads/";
+				$filename1 = strtolower(urlencode(basename($_FILES['fileUpload']['name'])));
+				$dest_path1 = $target_path . $filename1;
+				$dest_path2 = $target_path . 'tmarked.png';
+				if (!move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dest_path1))
+					echo '<h1>'.$_FILES["source"]["fileUpload"].' failed to load!</h1>';
+				else
+				{
+					
+				    echo "Stored in: " .$dest_path1.'<br>';	  
+					echo '<image src="'.$dest_path1. '" width = "400"></image>';
+					exec('java -cp uploads/ -jar uploads/dct-watermark-rev24.jar e -d '.$dest_path1.' '.$dest_path2.' "Hello World"');
+					echo '<image src="'.$dest_path2. '" width = "400"></image><br>';
+				}
+			}
+?>
 		
-	<?php	if (defined($_FILES) && $_FILES['fileUpload']): ?>
+	<?php	if (defined($_FILES) && $_FILES["fileUpload"]): ?>
 			<script>console.log($_FILES['fileUpload'])</script>
 			<br><image src="http://waterthemark.herokuapp.com/uploads/OrigLena.png"></image>
 	<?php else: ?>
