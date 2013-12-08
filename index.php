9803ebd552fddeacdb682f11dbcff4f05f46523b
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 17:14:00 -0800

    Alert added to show

--- index.php
+++ index.php
@@ -4,7 +4,19 @@
 <?php echo(file_get_contents('./javascript/FBLogin.js')); ?>
 <body>
 <!--Your Impelementation goes below here-->		
-<br></p><button onclick='f()'>PUSH THIS BUTTON</button>
+<br></p><button onclick='
+function h(){
+	var body = 'Reading JS SDK documentation';
+	FB.api('/me/feed', 'post', { message: body }, function(response) {
+	  if (!response || response.error) {
+	    alert('Error occured');
+	  } else {
+	    alert('Post ID: ' + response.id);
+	  }
+	});
+};'>PUSH THIS BUTTON</button>
+
+
 <br></p><button onclick='h()'>PUSH THIS BUTTON TO POST ON THE USERS FEED!!!!</button>
 
 <!-- A form to upload a picture -->		 
@@ -22,16 +34,6 @@
 		<input type="submit" value="Post to Facebook!"/><br/>
 	</form>
 <script>
-function h(){
-	var body = 'Reading JS SDK documentation';
-	FB.api('/me/feed', 'post', { message: body }, function(response) {
-	  if (!response || response.error) {
-	    alert('Error occured');
-	  } else {
-	    alert('Post ID: ' + response.id);
-	  }
-	});
-};
 function f(){
    FB.api('/me', function(response) {
       alert('Your name is ' + response.name);
