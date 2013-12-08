2a40d69a69e8073739f3f335c9f350a581ed3a29
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 21:39:34 -0800

    Trying to use java or php to control html object existence

--- index.php
+++ index.php
@@ -21,14 +21,15 @@
 	</form>
 	
 		
-<?php	if (defined($_FILES) && $_FILES['fileUpload']): ?>
+	<?php	if (defined($_FILES) && $_FILES['fileUpload']): ?>
 			<script>console.log($_FILES['fileUpload'])</script>
 			<br><image src="http://waterthemark.herokuapp.com/uploads/OrigLena.png"></image>
 	<?php else: ?>
 			<script>console.log("$_FILES undefined");</script>
 	<?php endif; ?>
-		
+	
 <!-- A form to generate an HTTP Post -->
+<!--
 	<form enctype="multipart/form-data" action="'.$graph_url.'" method="POST">
 		<datalist>
 		<option name="source" type="file" value="'.$_POST["source"].'"></option>
@@ -36,6 +37,7 @@
 		Say something about this photo: <input name="message" type="text" value=""><br/><br/>
 		<input type="submit" value="Post to Facebook!"/><br/>
 	</form>
+-->
 
 </body>
 <footer>
