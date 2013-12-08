8245e9035f1a971b005ca59fcd90c1ad7b6dff30
Chris Murphy <christoph2005@yahoo.com>
2013-12-08 02:09:33 -0800

    Making the layout make more sense

--- index.php
+++ index.php
@@ -17,24 +17,7 @@
 		<input type="submit" value="Watermark this image!"/><br>
 	</form>
 <!-- Show a Post To Facebook button if you find an uploaded filename -->
-<script>
-	function postToFacebook()
-	{
-		var body = '<?php echo $_POST["message"]?>';
-		var surl = 'http://waterthemark.herokuapp.com/<?php echo $dest_path2;?>';
-		FB.api('/me/photos', 'post', { message: body, url: surl }, function(response) {
-			if (!response || response.error) {
-		    	alert('Error occured: Probably not logged into Facebook');
-		    	console.log(response.error);
-		    	console.log(concat('URL:',surl));
-			} else {
-				alert('Post ID: ' + response.id);
-			}
-		});
-	}
-	document.getElementById("PostToFaceBook").removeAttribute("disabled");
-</script>
-<button type="button" id="PostToFacebook" onclick="postToFacebook()" disabled>Post To Facebook!</button><br>
+
 		
 	<?php if (!$_FILES || !$_FILES["FU"]): ?>
 		<script>console.log("$_FILES does not exist")</script>
@@ -78,10 +61,28 @@
 				echo '<image src="'.$dest_path1. '" style="max-height:300px;max-width:300px;"></image>';
 				# display the watermarked image
 				echo '<image src="'.$dest_path2. '" style="max-height:300px;max-width:300px;"></image><br>';
-				echo '<script>document.getElementById("PostToFaceBook").removeAttribute("disabled");</script>';
-				
 			}
 		}?>	
+		<?php
+		 if($_FILES["FU"]["name"]): ?>
+			<script>
+			function postToFacebook()
+			{
+				var body = '<?php echo $_POST["message"]?>';
+				var surl = 'http://waterthemark.herokuapp.com/<?php echo $dest_path2;?>';
+				FB.api('/me/photos', 'post', { message: body, url: surl }, function(response) {
+					if (!response || response.error) {
+				    	alert('Error occured: Probably not logged into Facebook');
+				    	console.log(response.error);
+				    	console.log(concat('URL:',surl));
+					} else {
+						alert('Post ID: ' + response.id);
+					}
+				});
+			}
+			</script>
+			<button type="button" onclick="postToFacebook()">Post To Facebook!</button><br>
+		<?php endif; ?>
 	<?php endif; ?>
 	
 <!-- A form to generate an HTTP Post -->
