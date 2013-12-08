50dde47d4831f039742285634d671514a955ae07
Chris Murphy <christoph2005@yahoo.com>
2013-12-08 00:33:38 -0800

    Making the layout make more sense

--- upload.php
+++ upload.php
@@ -15,7 +15,7 @@
 <!--Your Impelementation goes below here-->
 <!-- A form to upload a picture -->		 
 	<form id="UploadForm" enctype="multipart/form-data" action="./upload.php" method="POST">
-		<label for="FU">Please choose a photo you would like to watermark:</label>
+		<label for="FU">Choose a different image:</label>
 			<input name="FU" type="file"><br>
 		<label for="message">Post image with message:</label>
 			<input name="message" type="text" value='<?php echo $_POST["message"]?>'><br>
