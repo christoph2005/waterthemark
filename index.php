1623ab873a4c5d7c3e370c6560a7a217cba172d0
Chris Murphy <christoph2005@yahoo.com>
2013-12-07 22:22:00 -0800

    Trying to get the file to upload

--- index.php
+++ index.php
@@ -16,41 +16,42 @@
 <!-- A form to upload a picture -->		 
 	<form enctype="multipart/form-data" action="./index.php" method="POST">
 		Please choose a photo you would like to watermark:
-			<br><input name='fileUpload' type="file"><br><br>
+			<br><input name="fileUpload" type="file"><br><br>
 		<input type="submit" value="Upload Image For Watermarking"/><br>
 	</form>
-
+	
+	<?php #Print information about the uploaded file
+	if ($_FILES["fileUpload"]["error"] > 0)
+	{
+		echo "Error: " . $_FILES["fileUpload"]["error"] . "<br>";
+	}
+	else
+	{
+		echo "Upload: " . $_FILES["fileUpload"]["name"] . "<br>";
+		echo "Type: " . $_FILES["fileUpload"]["type"] . "<br>";
+		echo "Size: " . ($_FILES["fileUpload"]["size"] / 1024) . " kB<br>";
+		echo "Stored in: " . $_FILES["fileUpload"]["tmp_name"] . "<br>";
 		
+		// Note to self: $destpath junk can probably be tidied up a bit
+ 		$target_path = "uploads/";
+		$filename1 = strtolower(urlencode(basename($_FILES['fileUpload']['name'])));
+		$dest_path1 = $target_path . $filename1;
+		$dest_path2 = $target_path . 'tmarked.png';
+		if (!move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dest_path1))
+			echo '<h1>'.$_FILES["source"]["fileUpload"].' failed to load!</h1>';
+		else
+		{
+			
+		    echo "Stored in: " .$dest_path1.'<br>';	  
+			echo '<image src="'.$dest_path1. '" width = "400"></image>';
+			exec('java -cp uploads/ -jar uploads/dct-watermark-rev24.jar e -d '.$dest_path1.' '.$dest_path2.' "Hello World"');
+			echo '<image src="'.$dest_path2. '" width = "400"></image><br>';
+		}
+	}
+	?>
+			
 	<?php	if (defined($_FILES) && $_FILES["fileUpload"]): ?>
 			<script>console.log($_FILES['fileUpload'])</script>
-			<?php if ($_FILES["fileUpload"]["error"] > 0)
-			{
-				echo "Error: " . $_FILES["fileUpload"]["error"] . "<br>";
-			}
-			else
-			{
-				echo "Upload: " . $_FILES["fileUpload"]["name"] . "<br>";
-				echo "Type: " . $_FILES["fileUpload"]["type"] . "<br>";
-				echo "Size: " . ($_FILES["fileUpload"]["size"] / 1024) . " kB<br>";
-				echo "Stored in: " . $_FILES["fileUpload"]["tmp_name"] . "<br>";
-				
-				// Note to self: $destpath junk can probably be tidied up a bit
-		 		$target_path = "uploads/";
-				$filename1 = strtolower(urlencode(basename($_FILES['fileUpload']['name'])));
-				$dest_path1 = $target_path . $filename1;
-				$dest_path2 = $target_path . 'tmarked.png';
-				if (!move_uploaded_file($_FILES['fileUpload']['tmp_name'], $dest_path1))
-					echo '<h1>'.$_FILES["source"]["fileUpload"].' failed to load!</h1>';
-				else
-				{
-					
-				    echo "Stored in: " .$dest_path1.'<br>';	  
-					echo '<image src="'.$dest_path1. '" width = "400"></image>';
-					exec('java -cp uploads/ -jar uploads/dct-watermark-rev24.jar e -d '.$dest_path1.' '.$dest_path2.' "Hello World"');
-					echo '<image src="'.$dest_path2. '" width = "400"></image><br>';
-				}
-			}
-			?>
 			<br><image src="http://waterthemark.herokuapp.com/uploads/OrigLena.png"></image>
 	<?php else: ?>
 			<script>console.log("$_FILES undefined");</script>
